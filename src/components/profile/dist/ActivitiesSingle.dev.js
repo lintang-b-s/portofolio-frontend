3541708e41a09ed7f5f14216160cbee55ec2932f
protagonist77 <lintangbirdasaputra@gmail.com>
2022-10-22 13:40:35 +0700

    deploy

--- src/components/profile/dist/ActivitiesSingle.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _framerMotion = require("framer-motion");
-
-var _reactRouterDom = require("react-router-dom");
-
-var _propTypes = _interopRequireDefault(require("prop-types"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/customHooks/useScrollToTop.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-function useScrollToTop() {
-  useEffect(() => {
-    window.scrollTo(0, 0, "smooth");
-  }, []);
-}
-
-export default useScrollToTop;
